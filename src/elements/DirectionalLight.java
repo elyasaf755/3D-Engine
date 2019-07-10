f69e1ba18b56c3736356cb1d7df7534c64d5bd9b
barakamib <barakamib@gmail.com>
2019-07-10 17:35:52 +0300

    Documentation

--- src/elements/DirectionalLight.java
+++ src/elements/DirectionalLight.java
@@ -4,22 +4,39 @@ import primitives.Color;
 import primitives.Point3D;
 import primitives.Vector3D;
 
+/**
+ * Light with direction without a close source (eg sun)
+ */
 public class DirectionalLight extends Light implements LightSource {
-    protected Vector3D _direction;//direction of light without source (like sun)
+    protected Vector3D _direction;//direction of light
 
 
     //Constructors
 
+    /**
+     * constructor
+     * @param direction of light
+     */
     public DirectionalLight(Vector3D direction){
         super();
         _direction = new Vector3D(direction);
     }
 
+    /**
+     * constructor
+     * @param color (from local class 'color')
+     * @param direction of light
+     */
     public DirectionalLight(Color color, Vector3D direction){
         super(color);
         _direction = new Vector3D(direction);
     }
 
+    /**
+     * constructor
+     * @param color (from java.awt.Color)
+     * @param direction of light
+     */
     public DirectionalLight(java.awt.Color color, Vector3D direction){
         super(color);
         _direction = new Vector3D(direction);
--- src/elements/Light.java
+++ src/elements/Light.java
@@ -3,26 +3,43 @@ package elements;
 import primitives.Color;
 import primitives.Point3D;
 
+/**
+ * light
+ */
 public abstract class Light {
     protected Color _color;
 
 
     //Constructors
 
+    /**
+     * default constructor
+     */
     public Light(){
         _color = new Color(java.awt.Color.WHITE);
     }
 
+    /**
+     * constructor
+     * @param color (from local class 'color')
+     */
     public Light(Color color){
         _color = new Color(color);
     }
 
+    /**
+     * constructor
+     * @param color (from java.awt.Color)
+     */
     public Light(java.awt.Color color){
         _color = new Color(color);
     }
 
     //Methods
 
-    //return the color of the light
+    /**
+     * abstract
+     * @return color of light
+     */
     public abstract Color getIntensity();
 }
